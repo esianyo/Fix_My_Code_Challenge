7dba286dfed8a8d0fa9fe51220dd9d4b09a3c418
esianyo <esianicd@gmail.com>
2023-10-26 00:56:06 +0000

    gone

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -20,7 +20,7 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
@@ -30,4 +30,3 @@ end
 
 result.sort!
 puts result
-
